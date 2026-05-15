bebc5d006dbdad6ac637c0be925030bb5c447e69
Mjes9 <meri.estes@stu.barren.kyschools.us>
2026-05-15 11:14:09 -0500

    Added styles and fixed collection array

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const collection = [  { name: "Cameron", franchise: "Camera Verse", gen: 1 },  { name: "Mjes", franchise: "Camera Verse", gen: 1 },  { name: "Credence", franchise: "TMJ", gen: 1 },  { name: "Katie", franchise: "TMJ", gen: 1 },];
-
+const collection = [  { name: "Cameron", franchise: "Camera Verse", rating: 6, notes: "Cool Starlight", img: "https://via.placeholder.com/150" },  { name: "Mjes", franchise: "Camera Verse", rating: 7, notes: "Cool Valgonian", img: "https://via.placeholder.com/150" },  { name: "Credence", franchise: "TMJ", rating: 10, notes: "Cute bean", img: "https://via.placeholder.com/150" },  { name: "Katie", franchise: "TMJ", rating: 9, notes: "Cool gamer girl", img: "https://via.placeholder.com/150" }];
+//RENDER FUNCTION
 function renderCollection(items) {
 const display = document.getElementById('collection-display');
 display.innerHTML = ''; // clear everything
@@ -9,6 +9,8 @@ display.innerHTML += `
 <h3>${item.name}</h3>
 <p>Franchise: ${item.franchise}</p>
 <p>Rating: ${item.rating}/10</p>
+<p>Notes: ${item.notes}</p>
+<img src="${item.img || 'https://via.placeholder.com/150'}" alt="${item.name}">
 </div>
 `;
 });
